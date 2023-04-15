fbf35e1f0131ff28eb961a176352166d87bd2d29
CrezyDoll <126459834+CrezyDoll@users.noreply.github.com>
2023-04-15 16:04:33 +0530

    Update help.py

--- AnonX/plugins/help.py
+++ AnonX/plugins/help.py
@@ -60,7 +60,7 @@ async def helper_private(
         _ = get_string(language)
         keyboard = help_pannel(_)
 
-
+        await update.reply_photo(
             photo=config.START_IMG_URL,
             caption=_["help_1"].format(config.SUPPORT_HEHE), reply_markup=keyboard)
 
@@ -74,7 +74,7 @@ async def helper_private(
 @LanguageStart
 async def help_com_group(client, message: Message, _):
     keyboard = private_help_panel(_)
-
+    await message.reply_photo(
         photo=config.START_IMG_URL,
         caption=_["help_2"], reply_markup=InlineKeyboardMarkup(keyboard)
     )
