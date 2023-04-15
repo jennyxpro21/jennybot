8bc637e5bb97e974e03cd07989552c9261d6939a
CrezyDoll <126459834+CrezyDoll@users.noreply.github.com>
2023-04-15 19:37:18 +0530

    Update start.py

--- AnonX/plugins/start.py
+++ AnonX/plugins/start.py
@@ -44,7 +44,7 @@ async def start_comm(client, message: Message, _):
         if name[0:4] == "help":
             keyboard = help_pannel(_)
 
-
+            return await message.reply_photo(
                        photo=config.START_IMG_URL,
                        caption=_["help_1"].format(config.SUPPORT_HEHE), reply_markup=keyboard
             )
