f51955d49d2198ea4fad9a96cd4e0fda1a25ec62
Jenny x pro <123347744+jennyxpro21@users.noreply.github.com>
2023-07-04 20:41:43 +0530

    Update config.py

--- config.py
+++ config.py
@@ -26,7 +26,7 @@ UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv("GIT_TOKEN", None)
 
 SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/ITZ_DIL_KI_BAAT")
-SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/jenny_support")
+SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/HOTTY_SUPPORT")
 
 SUPPORT_HEHE = SUPPORT_GROUP.split("me/")[1]
 
